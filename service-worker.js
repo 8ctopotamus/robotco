d528a5fccdf739732bc5abe6a8334d275ee432ea
Josh <zylo.codes@gmail.com>
2022-06-30 20:09:42 -0500

    SW data strategy

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const STATIC_CACHE = 'static_cache_v2'
+const STATIC_CACHE = 'static_cache_v1'
 const DATA_CACHE = 'data_cache_v1'
 
 const FILES_TO_CACHE = [
@@ -36,7 +36,10 @@ const FILES_TO_CACHE = [
   "src/js/particles.json",
 ]
 
+const swIcon = `🤖 (SW): `
+
 self.addEventListener('install', event => {
+  console.log(`${swIcon} Installing...`)
   event.waitUntil(
     caches.open(STATIC_CACHE)
       .then(cache => cache.addAll(FILES_TO_CACHE))
@@ -45,6 +48,7 @@ self.addEventListener('install', event => {
 })
 
 self.addEventListener('activate', event => {
+  console.log(`${swIcon} Activating...`)
   event.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
@@ -57,4 +61,39 @@ self.addEventListener('activate', event => {
     })
   )
   self.clients.claim() // active service worker to set itself as the controller for all clients within its scope: https://developer.mozilla.org/en-US/docs/Web/API/Clients/claim
+})
+
+self.addEventListener('fetch', event => {
+  console.log(`${swIcon} Requesting `, event.request)
+  // Requests for data
+  // Strategy: Network-first, fallback to cache
+  if (event.request.url.includes('dummyjson.com') && event.request.method === 'GET') {
+    event.respondWith(
+      // open caches
+      caches.open(DATA_CACHE)
+        .then(cache => {
+          // try network with a fetch request
+          return fetch(event.request)
+            .then(response => {
+              // if success
+              if (response.status === 200) {
+                // save response in cache
+                cache.put(event.request.url, response.clone())
+              }
+              return response
+            })
+            // if fails pull last saved data from cache
+            .catch(() => caches.match(event.request))
+        })
+        .catch(err => console.log(err))
+    )
+    return
+  }
+
+  // Request for static assets (.html, .css, .js, .jpg, .png)
+  // Strategy: Cache-first, fallback to Network
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  )
 })
